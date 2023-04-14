a88339b4f8e460017936473462731e02646c801d
lmxti <matias.san1901@alumnos.ubiobio.cl>
2023-04-14 13:36:45 -0400

    UPDATE V1

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,170 @@
-import { useState } from 'react'
-import './App.css'
+// Hoja de estilo importada
+import "./App.css";
+
+// Hooks importados
+import { useEffect, useState } from "react";
+
+// Componentes de Material UI importados
+import {
+  Box,
+  Grid,
+  ListItem,
+  List,
+  Container,
+  Button,
+  Avatar,
+  Divider,
+} from "@mui/material";
+
+// Librerias importadas
+import dogNames from "dog-names";
+import LoremIpsum from "react-lorem-ipsum";
+import axios from "axios";
+
+// Iconos importados
+import logo from "./img/logo.png";
+import iconSetting from "../src/img/icon-settings.svg";
+import iconChat from "../src/img/icon-chat.png";
 
 function App() {
+  // Seteo de perro con propiedades de nombre e imagen.
+  const [perro, setPerro] = useState({ nombre: "", imagen: "" });
+  // Seteo de listado de perros que le gustaron
+  const [liked, setLiked] = useState([]);
+  // Seteo de listado de perros que no le gustaron
+  const [notLiked, setNotLiked] = useState([]);
+
+  // Funcion para obtener perro y setear perro (Nombre y foto aleatoria)
+  const getPerro = () => {
+    axios.get("https://dog.ceo/api/breeds/image/random").then((response) => {
+      setPerro({
+        nombre: dogNames.allRandom(),
+        imagen: response.data.message,
+      });
+    });
+  };
+
+  // Obtencion de perro al cargar la pagina
+  useEffect(() => {
+    getPerro();
+  }, []);
+
+  // Funcion para agregar a listado de liked (perros que le gustaron)
+  const giveLike = (perro) => {
+    //  Push de perro a lista de gustados
+    setLiked([...liked, perro]);
+    // Obtencion de nuevo perro
+    getPerro();
+  };
+
+  // Funcion para agregar a listado de notLiked (perros que no le gustaron)
+  const giveNotLike = (perro) => {
+    // Push de perro a lista de no gustados
+    setNotLiked([...notLiked, perro]);
+    // Obtencion de nuevo perro
+    getPerro();
+  };
+
   return (
     <>
-    <h1>hola mundo</h1>
+      <Container
+        style={{
+          backgroundColor: "whitesmoke",
+          marginTop: "100px",
+          padding: "50px",
+        }}
+      >
+        {/* Contenedor grid */}
+        <Grid container justifyContent="space-evenly">
+          {/* Lista de notLiked*/}
+          <Grid
+            bgcolor={"#fff"}
+            color={"black"}
+            xs={3}
+            className="shadow-border"
+          >
+            <List>
+              {notLiked.map((perro) => (
+                <ListItem className="list-item">
+                  <Avatar
+                    src={perro.imagen}
+                    alt="Imagen Avatar"
+                    style={{ marginRight: "10px" }}
+                  />
+                  {perro.nombre}
+                  <Button>Cambiar</Button>
+                </ListItem>
+              ))}
+            </List>
+          </Grid>
+
+          {/* Perfil de perro */}
+          <Grid
+            bgcolor={"#fff"}
+            item
+            xs={4}
+            className=" box-perfil shadow-border "
+          >
+            <Box className="div-header">
+              <img className="side-icon" src={iconSetting}></img>
+              <img className="logo" src={logo} alt="xd" />
+              <img className="side-icon" src={iconChat} alt="" />
+            </Box>
+            <Divider />
+            <Box className="container">
+              <img
+                className="img-perro"
+                src={perro.imagen}
+                alt="Imagen de perfil"
+              />
+              <h1 className="name-age">
+                {perro.nombre}, {Math.round(Math.random() * 4) + 1}
+              </h1>
+              <Divider />
+              <div className="lorem">
+                <LoremIpsum
+                  random
+                  startWithLoremIpsum={false}
+                  avgWordsPerSentence={2}
+                  avgSentencesPerParagraph={1}
+                />
+              </div>
+              <Box className="btns">
+                <div
+                  className="btn not-like"
+                  onClick={() => giveNotLike(perro)}
+                ></div>
+                <div className="btn like" onClick={() => giveLike(perro)}></div>
+              </Box>
+            </Box>
+          </Grid>
+
+          {/* Lista de liked */}
+          <Grid
+            bgcolor={"#fff"}
+            color={"black"}
+            xs={3}
+            className="shadow-border"
+          >
+            <List>
+              {liked.map((perro) => (
+                <ListItem className="list-item">
+                  <Avatar
+                    src={perro.imagen}
+                    alt="Imagen Avatar"
+                    style={{ marginRight: "10px" }}
+                  />
+                  {perro.nombre}
+                  <Button>Cambiar</Button>
+                </ListItem>
+              ))}
+            </List>
+          </Grid>
+          
+        </Grid>
+      </Container>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
